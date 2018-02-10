ab85bc038510680ae8db4c70797ad8ea183c1bd1
samrudapobbathi <spobbathi@hawk.iit.edu>
2018-02-10 14:54:01 -0600

    clean corpus script

---
+++ r_script.R
@@ -0,0 +1,36 @@
+setwd("~/Documents/uncommonhacks/toxic-comments-kaggle")
+require("tm")
+require("stringr")
+require("dplyr")
+
+data <- read.csv("data/train.csv")
+
+library(caTools)
+
+smp_siz = floor(0.75*nrow(data))
+set.seed(123)
+index <- sample(seq_len(nrow(data)),size = smp_siz)
+train_data <- data[index,]
+test_data <- data[-index,]
+
+data_corpus <- select(test_data,id,comment_text)
+colnames(data_corpus) <- c("doc_id","text")
+data_corpus <- VCorpus(DataframeSource(data_corpus))
+
+RemoveEmail <- function(x) {
+  str_replace_all(x,"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+", "")
+}
+
+data_corpus <- tm_map(data_corpus,content_transformer(RemoveEmail))
+data_corpus <- tm_map(data_corpus,removePunctuation)
+data_corpus <- tm_map(data_corpus,removeNumbers)
+data_corpus <- tm_map(data_corpus,stripWhitespace)
+data_corpus <- tm_map(data_corpus,removePunctuation)
+data_corpus <- tm_map(data_corpus,stripWhitespace)
+data_corpus <- tm_map(data_corpus,stemDocument)
+data_corpus <- tm_map(data_corpus,removeWords,stopwords("english"))
+data_corpus <- tm_map(data_corpus,stripWhitespace)
+
+data_matrix <- DocumentTermMatrix(data_corpus,control = list(bounds = list(global = c(0.01, 0.8))))
+
+library(lda)
